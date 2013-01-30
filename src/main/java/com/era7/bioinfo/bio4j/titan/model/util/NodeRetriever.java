19b50473d7de1b3808673f2cc70ffa36e6b9859e
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2013-01-30 12:28:47 +0100

    some progress on Titan related classes

--- src/main/java/com/era7/bioinfo/bio4j/titan/model/util/NodeRetriever.java
+++ src/main/java/com/era7/bioinfo/bio4j/titan/model/util/NodeRetriever.java
@@ -18,6 +18,9 @@ package com.era7.bioinfo.bio4j.titan.model.util;
 
 import com.era7.bioinfo.bio4j.blueprints.model.nodes.DatasetNode;
 import com.era7.bioinfo.bio4j.blueprints.model.nodes.EnzymeNode;
+import com.era7.bioinfo.bio4j.blueprints.model.nodes.GoTermNode;
+import com.era7.bioinfo.bio4j.blueprints.model.nodes.ProteinNode;
+import com.era7.bioinfo.bio4j.blueprints.model.nodes.refseq.GenomeElementNode;
 import com.tinkerpop.blueprints.Vertex;
 import java.util.Iterator;
 
@@ -25,52 +28,115 @@ import java.util.Iterator;
  *
  * @author Pablo Pareja Tobes <ppareja@era7.com>
  */
-public class NodeRetriever {   
-    
-    
+public class NodeRetriever {
+
     protected Bio4jManager manager;
-    
-    public NodeRetriever(Bio4jManager bio4jManager){
+
+    public NodeRetriever(Bio4jManager bio4jManager) {
         manager = bio4jManager;
     }
-    
+
     //-------------------------------------------------------------------
     //--------------------------ENZYME-----------------------------------
-    
-    public EnzymeNode getEnzymeById(String id){        
-        Iterator<Vertex> iterator = manager.getGraph().getVertices(EnzymeNode.ID_PROPERTY, id).iterator(); 
-        if(iterator.hasNext()){
+    public EnzymeNode getEnzymeById(String id) {
+        Iterator<Vertex> iterator = manager.getGraph().getVertices(EnzymeNode.ID_PROPERTY, id).iterator();
+        if (iterator.hasNext()) {
             return new EnzymeNode(iterator.next());
-        }else{
+        } else {
             return null;
         }
     }
-    
+
     //-------------------------------------------------------------------
     //--------------------------DATASETS-----------------------------------
-        
-    public DatasetNode getDatasetByName(String name){
-        Iterator<Vertex> iterator = manager.getGraph().getVertices(DatasetNode.NAME_PROPERTY, name).iterator();  
-        if(iterator.hasNext()){
+    public DatasetNode getDatasetByName(String name) {
+        Iterator<Vertex> iterator = manager.getGraph().getVertices(DatasetNode.NAME_PROPERTY, name).iterator();
+        if (iterator.hasNext()) {
             return new DatasetNode(iterator.next());
-        }else{
-            return null;
-        }
-    }    
-    public DatasetNode getSwissProtDataset(){
-        Iterator<Vertex> iterator = manager.getGraph().getVertices(DatasetNode.NAME_PROPERTY, DatasetNode.SWISS_PROT_DATASET_NAME).iterator();  
-        if(iterator.hasNext()){
-            return new DatasetNode(iterator.next());
-        }else{
+        } else {
             return null;
         }
     }
-    public DatasetNode getTremblDataset(){
-        Iterator<Vertex> iterator = manager.getGraph().getVertices(DatasetNode.NAME_PROPERTY, DatasetNode.TREMBL_DATASET_NAME).iterator();  
-        if(iterator.hasNext()){
+
+    public DatasetNode getSwissProtDataset() {
+        Iterator<Vertex> iterator = manager.getGraph().getVertices(DatasetNode.NAME_PROPERTY, DatasetNode.SWISS_PROT_DATASET_NAME).iterator();
+        if (iterator.hasNext()) {
             return new DatasetNode(iterator.next());
-        }else{
+        } else {
             return null;
         }
+    }
+
+    public DatasetNode getTremblDataset() {
+        Iterator<Vertex> iterator = manager.getGraph().getVertices(DatasetNode.NAME_PROPERTY, DatasetNode.TREMBL_DATASET_NAME).iterator();
+        if (iterator.hasNext()) {
+            return new DatasetNode(iterator.next());
+        } else {
+            return null;
+        }
+    }
+
+    //-------------------------------------------------------------------
+    //--------------------------REFSEQ-----------------------------------    
+    public GenomeElementNode getGenomeElementByVersion(String version) {
+        Iterator<Vertex> iterator = manager.getGraph().getVertices(GenomeElementNode.VERSION_PROPERTY, version).iterator();
+        if (iterator.hasNext()) {
+            return new GenomeElementNode(iterator.next());
+        } else {
+            return null;
+        }
+    }
+
+    //-------------------------------------------------------------------
+    //--------------------GENE ONTOLOGY--------------------------------    
+    /**
+     *
+     * @param goId
+     * @return GoTermNode with the id provided
+     */
+    public GoTermNode getGoTermById(String goId) {
+        Iterator<Vertex> iterator = manager.getGraph().getVertices(GoTermNode.ID_PROPERTY, goId).iterator();
+        if (iterator.hasNext()) {
+            return new GoTermNode(iterator.next());
+        } else {
+            return null;
+        }
+    }
+    //-------------------------------------------------------------------
+    //--------------------PROTEINS--------------------------------
+
+    /**
+     *
+     * @param proteinAccession
+     * @return ProteinNode with the accession provided
+     */
+    public ProteinNode getProteinNodeByAccession(String proteinAccession) {
+        Iterator<Vertex> iterator = manager.getGraph().getVertices(ProteinNode.ACCESSION_PROPERTY, proteinAccession).iterator();
+        if (iterator.hasNext()) {
+            return new ProteinNode(iterator.next());
+        } else {
+            //we check if we can find it as a an alternative accession
+            iterator = manager.getGraph().getVertices(ProteinNode.ALTERNATIVE_ACCESSIONS_PROPERTY, proteinAccession).iterator();
+            if (iterator.hasNext()) {
+                return new ProteinNode(iterator.next());
+            } else {
+                return null;
+            }
+        }
+    }
+    /**
+     * 
+     * @param ensemblPlantsRef 
+     * @return ProteinNode with the Ensembl Plants reference provided
+     */
+    public ProteinNode getProteinNodeByEnsemblPlantsRef(String ensemblPlantsRef){
+        
+        Iterator<Vertex> iterator = manager.getGraph().getVertices(ProteinNode.ENSEMBL_PLANTS_REFERENCES_PROPERTY, ensemblPlantsRef).iterator();
+        
+        if(iterator.hasNext()){
+            return new ProteinNode(iterator.next());
+        }else{
+            return null;
+        }        
     }
 }
