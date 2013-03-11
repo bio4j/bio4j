7638ccd01822fb4f21c943719b6c260b1a6f0de2
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2013-03-11 15:01:29 +0100

    making progress on model interfaces implementation

---
+++ src/main/java/com/era7/bioinfo/bio4j/model/nodes/citation/DB.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2010-2013  "Bio4j"
+ *
+ * This file is part of Bio4j
+ *
+ * Bio4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package com.era7.bioinfo.bio4j.model.nodes.citation;
+
+import com.era7.bioinfo.bio4j.model.nodes.BasicNode;
+import java.util.List;
+
+/**
+ *
+ * @author Pablo Pareja Tobes <ppareja@era7.com>
+ */
+public interface DB extends BasicNode{
+    
+    //----GETTERS---
+    public String getName();
+    public List<Submission> getAssociatedSubmissions();
+    
+    //----SETTERS----
+    public void setName(String value);
+    
+}
