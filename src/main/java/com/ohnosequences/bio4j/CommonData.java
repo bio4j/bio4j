8829db2cf9b117347cfcee428aab7629a12dc534
Alexey Alekhin <laughedelic@gmail.com>
2014-02-12 11:37:35 +0100

    Added CommonData

---
+++ src/main/java/com/ohnosequences/bio4j/CommonData.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2010-2011  "Bio4j"
+ *
+ * This file is part of Bio4j
+ *
+ * Bio4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.ohnosequences.bio4j;
+
+/**
+ * 
+ * @author Pablo Pareja Tobes <ppareja@era7.com>
+ */
+public class CommonData {
+    
+    public static final String DATABASE_FOLDER = "bio4jdb";
+
+    public static final String PROPERTIES_FILE_NAME = "batchInserter.properties";
+    public static final String ENTRY_TAG_NAME = "entry";
+    public static final String ENTRY_ACCESSION_TAG_NAME = "accession";
+    public static final String ENTRY_NAME_TAG_NAME = "name";
+    public static final String ENTRY_MODIFIED_DATE_ATTRIBUTE = "modified";
+    public static final String ENTRY_DATASET_ATTRIBUTE = "dataset";
+    public static final String ENTRY_SEQUENCE_TAG_NAME = "sequence";
+
+    public static final String KEYWORD_TAG_NAME = "keyword";
+    public static final String KEYWORD_ID_ATTRIBUTE = "id";
+
+    public static final String REFERENCE_TAG_NAME = "reference";
+    public static final String CITATION_TAG_NAME = "citation";
+
+    public static final String ORGANISM_TAG_NAME = "organism";
+    public static final String ORGANISM_NAME_TAG_NAME = "name";
+    public static final String ORGANISM_NAME_TYPE_ATTRIBUTE = "type";
+    public static final String ORGANISM_SCIENTIFIC_NAME_TYPE = "scientific";
+    public static final String ORGANISM_COMMON_NAME_TYPE = "common";
+    public static final String ORGANISM_SYNONYM_NAME_TYPE = "synonym";
+
+    public static final String DB_REFERENCE_TAG_NAME = "dbReference";
+    public static final String DB_REFERENCE_TYPE_ATTRIBUTE = "type";
+    public static final String DB_REFERENCE_ID_ATTRIBUTE = "id";
+    public static final String DB_REFERENCE_VALUE_ATTRIBUTE = "value";
+    public static final String DB_REFERENCE_PROPERTY_TAG_NAME = "property";
+
+    public static final String INTERPRO_DB_REFERENCE_TYPE = "InterPro";
+    public static final String INTERPRO_ENTRY_NAME = "entry name";
+
+    public static final String GO_DB_REFERENCE_TYPE = "GO";
+    public static final String EVIDENCE_TYPE_ATTRIBUTE = "evidence";
+    
+    public static final String SEQUENCE_MASS_ATTRIBUTE = "mass";
+    public static final String SEQUENCE_LENGTH_ATTRIBUTE = "length";
+    public static final String PROTEIN_TAG_NAME = "protein";
+    public static final String PROTEIN_RECOMMENDED_NAME_TAG_NAME = "recommendedName";
+    public static final String PROTEIN_FULL_NAME_TAG_NAME = "fullName";
+    public static final String PROTEIN_SHORT_NAME_TAG_NAME = "shortName";
+    public static final String GENE_TAG_NAME = "gene";
+    public static final String GENE_NAME_TAG_NAME = "name";
+    public static final String COMMENT_TAG_NAME = "comment";
+    public static final String COMMENT_TYPE_ATTRIBUTE = "type";
+    public static final String COMMENT_ALTERNATIVE_PRODUCTS_TYPE = "alternative products";
+    public static final String COMMENT_SEQUENCE_CAUTION_TYPE = "sequence caution";
+    public static final String SUBCELLULAR_LOCATION_TAG_NAME = "subcellularLocation";
+    public static final String LOCATION_TAG_NAME = "location";
+    public static final String COMMENT_TEXT_TAG_NAME = "text";
+    public static final String FEATURE_TAG_NAME = "feature";
+    public static final String FEATURE_TYPE_ATTRIBUTE = "type";
+    public static final String FEATURE_DESCRIPTION_ATTRIBUTE = "description";
+    public static final String STATUS_ATTRIBUTE = "status";
+    public static final String FEATURE_REF_ATTRIBUTE = "ref";
+    public static final String FEATURE_ID_ATTRIBUTE = "id";
+    public static final String EVIDENCE_ATTRIBUTE = "evidence";
+    public static final String FEATURE_LOCATION_TAG_NAME = "location";
+    public static final String FEATURE_ORIGINAL_TAG_NAME = "original";
+    public static final String FEATURE_VARIATION_TAG_NAME = "variation";
+    public static final String FEATURE_POSITION_TAG_NAME = "position";
+    public static final String FEATURE_LOCATION_BEGIN_TAG_NAME = "begin";
+    public static final String FEATURE_LOCATION_END_TAG_NAME = "end";
+    public static final String FEATURE_LOCATION_POSITION_ATTRIBUTE = "position";
+    public static final String FEATURE_POSITION_POSITION_ATTRIBUTE = "position";
+       
+    
+    public static final String REFSEQ_BUCKET_NAME = "refseq-bucket";
+    
+
+}
