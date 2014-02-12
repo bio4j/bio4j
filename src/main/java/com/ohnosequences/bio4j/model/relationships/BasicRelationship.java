5368289f6cea0120d4dad14f282c93b81cb19583
Alexey Alekhin <laughedelic@gmail.com>
2014-02-12 11:24:56 +0100

    Removed all code; moved abstract part from bio4j/model; added sbt conf

---
+++ src/main/java/com/ohnosequences/bio4j/model/relationships/BasicRelationship.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ohnosequences.bio4j.model.relationships;
+
+/**
+ *
+ * @author Pablo Pareja Tobes <ppareja@era7.com>
+ */
+public interface BasicRelationship {
+    
+    public String getType();    
+    
+}
