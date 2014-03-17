5f2a850f80cbcef9c89bfd70b8726004d481db1c
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2014-03-17 18:11:50 +0100

    same for Nodes?

--- src/main/java/com/bio4j/model/relationships/IsoformEventGenerator.java
+++ src/main/java/com/bio4j/model/relationships/IsoformEventGenerator.java
@@ -10,7 +10,7 @@ import com.bio4j.model.nodes.Isoform;
  * @author Pablo Pareja Tobes <ppareja@era7.com>
  */
 public interface IsoformEventGenerator {
-    
+  
   public Isoform getIsoform();
   public AlternativeProduct getAlternativeProduct();
 }
