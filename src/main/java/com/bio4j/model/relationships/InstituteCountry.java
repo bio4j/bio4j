853cbbd89f60f8419699f285067e7324cfdd9147
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2014-03-25 15:49:19 +0100

    refactor 3 rels as a sample

--- src/main/java/com/bio4j/model/relationships/InstituteCountry.java
+++ src/main/java/com/bio4j/model/relationships/InstituteCountry.java
@@ -1,6 +1,7 @@
 package com.bio4j.model.relationships;
 
 import com.bio4j.model.Relationship;
+import com.bio4j.model.RelationshipType;
 
 import com.bio4j.model.nodes.Country;
 import com.bio4j.model.nodes.Institute;
@@ -9,8 +10,23 @@ import com.bio4j.model.nodes.Institute;
  *
  * @author Pablo Pareja Tobes <ppareja@era7.com>
  */
-public interface InstituteCountry extends Relationship {
-    
+public interface InstituteCountry extends Relationship <
+  Institute, Institute.type,
+  InstituteCountry, InstituteCountry.type,
+  Country, Country.type
+> {
+  
+  enum type implements RelationshipType <
+    Institute, Institute.type,
+    InstituteCountry, InstituteCountry.type,
+    Country, Country.type
+  > {
+
+    instituteCountry;
+    public type value() { return instituteCountry; }
+  }
+
+  // replace this by source/target??
   public Institute getInstitute();    
-  public Country getCountry();    
+  public Country getCountry();
 }
--- src/main/java/com/bio4j/model/relationships/TaxonParent.java
+++ src/main/java/com/bio4j/model/relationships/TaxonParent.java
@@ -1,9 +1,23 @@
 package com.bio4j.model.relationships;
 
 import com.bio4j.model.Relationship;
+import com.bio4j.model.RelationshipType;
+import com.bio4j.model.nodes.Taxon;
 
-/**
- *
- * @author Pablo Pareja Tobes <ppareja@era7.com>
- */
-public interface TaxonParent extends Relationship {}
+public interface TaxonParent extends Relationship <
+  Taxon, Taxon.type,
+  TaxonParent, TaxonParent.type,
+  Taxon, Taxon.type
+> {
+    
+  enum type implements RelationshipType <
+    Taxon, Taxon.type,
+    TaxonParent, TaxonParent.type,
+    Taxon, Taxon.type
+  > {
+
+    taxonParent;
+    public type value() { return taxonParent; }
+  }
+
+}
