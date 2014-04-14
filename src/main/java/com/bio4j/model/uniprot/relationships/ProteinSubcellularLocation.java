fa843cd6469c846caeac2e9a45cb01bda985177d
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-04-14 15:34:30 +0200

    making some progress on refactor

--- src/main/java/com/bio4j/model/uniprot/relationships/ProteinSubcellularLocation.java
+++ src/main/java/com/bio4j/model/uniprot/relationships/ProteinSubcellularLocation.java
@@ -1,5 +1,9 @@
 package com.bio4j.model.uniprot.relationships;
 
+import com.bio4j.model.Relationship;
+import com.bio4j.model.RelationshipType;
+import com.bio4j.model.RelationshipType.Arity;
+import com.bio4j.model.properties.Evidence;
 import com.bio4j.model.uniprot.nodes.Protein;
 import com.bio4j.model.uniprot.nodes.SubcellularLocation;
 
@@ -7,19 +11,38 @@ import com.bio4j.model.uniprot.nodes.SubcellularLocation;
  *
  * @author Pablo Pareja Tobes <ppareja@era7.com>
  */
-public interface ProteinSubcellularLocation{
-    
-  public String getEvidence();
+public interface ProteinSubcellularLocation extends Relationship <
+	Protein, Protein.Type,
+	ProteinSubcellularLocation, ProteinSubcellularLocation.Type,
+	SubcellularLocation, SubcellularLocation.Type
+	>,
+	Evidence<ProteinSubcellularLocation, ProteinSubcellularLocation.Type>,
+	{
+	
+	public static Type TYPE = Type.proteinSubcellularLocation;
+	public static enum Type implements RelationshipType <
+		Protein, Protein.Type,
+		ProteinSubcellularLocation, ProteinSubcellularLocation.Type,
+		SubcellularLocation, SubcellularLocation.Type
+		  > {
+		
+			proteinSubcellularLocation;
+		   public Type value() { return proteinSubcellularLocation; }
+		   public Arity arity() { return Arity.manyToMany; }
+		   public Protein.Type sourceType() { return Protein.TYPE; }
+		   public SubcellularLocation.Type targetType() { return SubcellularLocation.TYPE; }
+	}
+		
+	public Protein source();
+	public SubcellularLocation target();
+
+}
   public String getStatus();
   public String getTopology();
   public String getTopologyStatus();
 
-  public void setEvidence(String value);
   public void setStatus(String value);
   public void setTopology(String value);
   public void setTopologyStatus(String value);
 
 
-  public Protein getProtein();    
-  public SubcellularLocation getSubcellularLocation();
-}
