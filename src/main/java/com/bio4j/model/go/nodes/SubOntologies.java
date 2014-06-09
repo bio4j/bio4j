2b0d9e296d56b19d0d5c0bdccbb08f71927a4464
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-06-09 10:00:36 +0200

    updating subontolgies

--- src/main/java/com/bio4j/model/go/nodes/SubOntologies.java
+++ src/main/java/com/bio4j/model/go/nodes/SubOntologies.java
@@ -2,8 +2,9 @@ package com.bio4j.model.go.nodes;
 
 import java.util.List;
 
-
+// base types
 import com.ohnosequences.typedGraphs.Node;
+import com.ohnosequences.typedGraphs.Property;
 
 import com.bio4j.model.go.relationships.MolecularFunction;
 import com.bio4j.model.go.relationships.BiologicalProcess;
@@ -18,24 +19,37 @@ import com.bio4j.model.go.GoGraph.SubOntologiesType;
  * @author <a href="mailto:eparejatobes@ohnosequences.com">Eduardo Pareja-Tobes</a>
  */
 public interface SubOntologies<
-  N extends SubOntologies<N,NT>,
-  NT extends SubOntologiesType<N,NT>
-> 
-  extends Node<N,NT>
-{
+        N extends SubOntologies<N, NT>,
+        NT extends SubOntologiesType<N, NT>
+        >
+        extends Node<N, NT> {
 
-  // MolecularFunction
-  // incoming
-  public List<? extends MolecularFunction> molecularFunction_in();
-  public List<? extends Term> term_inNodes();
-  
-  // BiologicalProcess
-  // incoming
-  public List<? extends BiologicalProcess> biologicalProcess_in(); 
-  public List<? extends Term> biologicalProcess_inNodes();
+    public static interface name <
+            N extends SubOntologies<N,NT>,
+            NT extends SubOntologiesType<N,NT>,
+            P extends name<N,NT,P>
+            >
+            extends Property<N,NT,P,String>
+    {
+        @Override public default String name() { return "name"; }
+        @Override public default Class<String> valueClass() { return String.class; }
+    }
 
-  // CellularComponent
-  // incoming
-  public List<? extends CellularComponent> cellularComponent_in(); 
-  public List<? extends Term> cellularComponent_inNodes();
+    // MolecularFunction
+    // incoming
+    public List<? extends MolecularFunction> molecularFunction_in();
+
+    public List<? extends Term> term_inNodes();
+
+    // BiologicalProcess
+    // incoming
+    public List<? extends BiologicalProcess> biologicalProcess_in();
+
+    public List<? extends Term> biologicalProcess_inNodes();
+
+    // CellularComponent
+    // incoming
+    public List<? extends CellularComponent> cellularComponent_in();
+
+    public List<? extends Term> cellularComponent_inNodes();
 }
