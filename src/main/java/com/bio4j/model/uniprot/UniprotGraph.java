2c72ac12c2e2af37f9724058c6cd2620832e61aa
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-07-28 10:00:37 +0200

    starting with the implementation of UniprotGraph

---
+++ src/main/java/com/bio4j/model/uniprot/UniprotGraph.java
@@ -0,0 +1,101 @@
+package com.bio4j.model.uniprot;
+
+import com.ohnosequences.typedGraphs.TypedGraph;
+import com.ohnosequences.typedGraphs.UntypedGraph;
+
+/**
+ * Created by ppareja on 7/28/2014.
+ */
+public abstract class UniprotGraph<
+		// untyped graph
+		I extends UntypedGraph<RV, RVT, RE, RET>,
+		// vertices
+		RV, RVT,
+		// edges
+		RE, RET
+		>
+		implements
+		TypedGraph<
+				UniprotGraph<I, RV, RVT, RE, RET>,
+				I, RV, RVT, RE, RET
+				> {
+
+	// types
+	// vertices
+	public abstract ArticleType Article();
+
+	public abstract BookType Book();
+
+	public abstract CityType City();
+
+	public abstract CommentTypeType CommentType();
+
+	public abstract ConsortiumType Consortium();
+
+	public abstract DatasetType Dataset();
+
+	public abstract CountryType Country();
+
+	public abstract DBType DB();
+
+	public abstract EMBLType EMBL();
+
+	public abstract EnsemblType Ensembl();
+
+	public abstract FeatureType FeatureType();
+
+	public abstract InstituteType Institute();
+
+	public abstract InterproType Interpro();
+
+	public abstract JournalType Journal();
+
+	public abstract KeggType Kegg();
+
+	public abstract KeywordType Keyword();
+
+	public abstract OnlineArticleType OnlineArticle();
+
+	public abstract OrganismType Organism();
+
+	public abstract OnlineJournalType OnlineJournal();
+
+	public abstract PatentType Patent();
+
+	public abstract PersonType Person();
+
+	public abstract PfamType Pfam();
+
+	public abstract PIRType PIR();
+
+	public abstract ProteinType Protein();
+
+	public abstract PublisherType Publisher();
+
+	public abstract PubmedType Pubmed();
+
+	public abstract ReactomeTermType ReactomeTerm();
+
+	public abstract ReferenceType Reference();
+
+	public abstract RefSeqType RefSeq();
+
+	public abstract SequenceCautionType SequenceCaution();
+
+	public abstract SubcellularLocationType SubcellularLocation();
+
+	public abstract SubmissionType Submission();
+
+	public abstract TaxonType Taxon();
+
+	public abstract ThesisType Thesis();
+
+	public abstract UniGeneType UniGene();
+
+	public abstract UnpublishedObservationType UnpublishedObservation();
+
+	// edges
+	public abstract PartOfType PartOf();
+
+
+}
