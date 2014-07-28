d005218c96440dbc862822f360a0d6083d31a9f2
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-07-28 10:39:06 +0200

    making some progress on the implementation of Uniprot module

--- src/main/java/com/bio4j/model/uniprot/UniprotGraph.java
+++ src/main/java/com/bio4j/model/uniprot/UniprotGraph.java
@@ -1,7 +1,7 @@
 package com.bio4j.model.uniprot;
 
-import com.ohnosequences.typedGraphs.TypedGraph;
-import com.ohnosequences.typedGraphs.UntypedGraph;
+import com.bio4j.model.uniprot.nodes.Article;
+import com.ohnosequences.typedGraphs.*;
 
 /**
  * Created by ppareja on 7/28/2014.
@@ -20,7 +20,9 @@ public abstract class UniprotGraph<
 				I, RV, RVT, RE, RET
 				> {
 
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// types
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// vertices
 	public abstract ArticleType Article();
 
@@ -94,8 +96,267 @@ public abstract class UniprotGraph<
 
 	public abstract UnpublishedObservationType UnpublishedObservation();
 
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// edges
-	public abstract PartOfType PartOf();
+
+	public abstract ArticlePubmedType ArticlePubmed();
+
+	public abstract ArticleJournalType ArticleJournalType();
+
+	public abstract BookCityType BookCity();
+
+	public abstract BookPublisherType BookPublisher();
+
+	public abstract InstituteCountryType InstituteCountry();
+
+	public abstract OnlineArticleJournalType OnlineArticleJournal();
+
+	public abstract OrganismTaxonType OrganismTaxon();
+
+	public abstract ProteinCommentType ProteinComment();
+
+	public abstract ProteinDatasetType ProteinDataset();
+
+	public abstract ProteinEMBLType ProteinEMBL();
+
+	public abstract ProteinEnsemblType ProteinEnsembl();
+
+	public abstract ProteinReferenceType ProteinReference();
+
+	public abstract ReferenceArticleType ReferenceArticle();
+
+	public abstract ReferenceBookType ReferenceBook();
+
+	public abstract ReferenceOnlineArticleType ReferenceOnlineArticle();
+
+	public abstract ReferenceThesisType ReferenceThesis();
+
+	public abstract ReferenceSubmissionType ReferenceSubmission();
+
+	public abstract ReferenceUnpublishedObservationType ReferenceUnpublishedObservation();
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Vertex types
+
+	public final class ArticleType
+			extends
+			UniprotVertexType<
+					Article<I, RV, RVT, RE, RET>,
+					UniprotGraph<I, RV, RVT, RE, RET>.ArticleType
+					> {
+
+		public final doId doId = new doId();
+		public final title title = new title();
+
+		protected ArticleType(RVT raw) {
+			super(raw);
+		}
+
+		@Override
+		public ArticleType value() {
+			return graph().Article();
+		}
+
+		@Override
+		public Article<I, RV, RVT, RE, RET> from(RV vertex) {
+			return new Article<I, RV, RVT, RE, RET>(vertex, this);
+		}
+
+		public final class doId
+				extends
+				UniprotVertexProperty<Article<I, RV, RVT, RE, RET>, ArticleType, doId, String> {
+			public doId() {
+				super(ArticleType.this.graph().Article());
+			}
+
+			public Class<String> valueClass() {
+				return String.class;
+			}
+		}
+
+		public final class title
+				extends
+				UniprotVertexProperty<Article<I, RV, RVT, RE, RET>, ArticleType, title, String> {
+			public title() {
+				super(ArticleType.this.graph().Article());
+			}
+
+			public Class<String> valueClass() {
+				return String.class;
+			}
+		}
+
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// helper classes
+
+	public abstract class UniprotVertexProperty<
+			V extends UniprotVertex<V, VT, I, RV, RVT, RE, RET>,
+			VT extends UniprotGraph<I, RV, RVT, RE, RET>.UniprotVertexType<V, VT>,
+			P extends UniprotVertexProperty<V, VT, P, PV>,
+			PV
+			>
+			implements
+			Property<V, VT, P, PV, UniprotGraph<I, RV, RVT, RE, RET>, I, RV, RVT, RE, RET> {
+
+		protected UniprotVertexProperty(VT type) {
+
+			this.type = type;
+		}
+
+		private VT type;
+
+		@Override
+		public final VT elementType() {
+			return type;
+		}
+	}
+
+	public abstract static class UniprotVertex<
+			V extends UniprotVertex<V, VT, I, RV, RVT, RE, RET>,
+			VT extends UniprotGraph<I, RV, RVT, RE, RET>.UniprotVertexType<V, VT>,
+			I extends UntypedGraph<RV, RVT, RE, RET>, RV, RVT, RE, RET
+			>
+			implements
+			TypedVertex<V, VT, UniprotGraph<I, RV, RVT, RE, RET>, I, RV, RVT, RE, RET> {
+
+		private RV vertex;
+		private VT type;
+
+		protected UniprotVertex(RV vertex, VT type) {
+
+			this.vertex = vertex;
+			this.type = type;
+		}
+
+		@Override
+		public UniprotGraph<I, RV, RVT, RE, RET> graph() {
+			return type().graph();
+		}
+
+		@Override
+		public RV raw() {
+			return this.vertex;
+		}
+
+		@Override
+		public VT type() {
+			return type;
+		}
+	}
+
+	abstract class UniprotVertexType<
+			V extends UniprotVertex<V, VT, I, RV, RVT, RE, RET>,
+			VT extends UniprotGraph<I, RV, RVT, RE, RET>.UniprotVertexType<V, VT>
+			>
+			implements
+			TypedVertex.Type<V, VT, UniprotGraph<I, RV, RVT, RE, RET>, I, RV, RVT, RE, RET> {
+
+		private RVT raw;
+
+		protected UniprotVertexType(RVT raw) {
+			this.raw = raw;
+		}
+
+		@Override
+		public final RVT raw() {
+			return raw;
+		}
+
+		@Override
+		public final UniprotGraph<I, RV, RVT, RE, RET> graph() {
+			return UniprotGraph.this;
+		}
+	}
+
+	public abstract static class UniprotEdge<
+			S extends UniprotVertex<S, ST, I, RV, RVT, RE, RET>,
+			ST extends UniprotGraph<I, RV, RVT, RE, RET>.UniprotVertexType<S, ST>,
+			E extends UniprotEdge<S, ST, E, ET, T, TT, I, RV, RVT, RE, RET>,
+			ET extends UniprotGraph<I, RV, RVT, RE, RET>.UniprotEdgeType<S, ST, E, ET, T, TT>,
+			T extends UniprotVertex<T, TT, I, RV, RVT, RE, RET>,
+			TT extends UniprotGraph<I, RV, RVT, RE, RET>.UniprotVertexType<T, TT>,
+			I extends UntypedGraph<RV, RVT, RE, RET>, RV, RVT, RE, RET
+			>
+			implements
+			TypedEdge<
+					S, ST, UniprotGraph<I, RV, RVT, RE, RET>,
+					E, ET, UniprotGraph<I, RV, RVT, RE, RET>, I, RV, RVT, RE, RET,
+					T, TT, UniprotGraph<I, RV, RVT, RE, RET>
+					> {
+
+		private RE edge;
+		private ET type;
+
+		protected UniprotEdge(RE edge, ET type) {
+
+			this.edge = edge;
+			this.type = type;
+		}
+
+		@Override
+		public UniprotGraph<I, RV, RVT, RE, RET> graph() {
+			return type().graph();
+		}
+
+		@Override
+		public RE raw() {
+			return this.edge;
+		}
+
+		@Override
+		public ET type() {
+			return type;
+		}
+	}
+
+	abstract class UniprotEdgeType<
+			S extends UniprotVertex<S, ST, I, RV, RVT, RE, RET>,
+			ST extends UniprotGraph<I, RV, RVT, RE, RET>.UniprotVertexType<S, ST>,
+			E extends UniprotEdge<S, ST, E, ET, T, TT, I, RV, RVT, RE, RET>,
+			ET extends UniprotGraph<I, RV, RVT, RE, RET>.UniprotEdgeType<S, ST, E, ET, T, TT>,
+			T extends UniprotVertex<T, TT, I, RV, RVT, RE, RET>,
+			TT extends UniprotGraph<I, RV, RVT, RE, RET>.UniprotVertexType<T, TT>
+			>
+			implements
+			TypedEdge.Type<
+					S, ST, UniprotGraph<I, RV, RVT, RE, RET>,
+					E, ET, UniprotGraph<I, RV, RVT, RE, RET>, I, RV, RVT, RE, RET,
+					T, TT, UniprotGraph<I, RV, RVT, RE, RET>
+					> {
+
+		private RET raw;
+		private ST srcT;
+		private TT tgtT;
+
+		protected UniprotEdgeType(ST srcT, RET raw, TT tgtT) {
+
+			this.raw = raw;
+			this.srcT = srcT;
+			this.tgtT = tgtT;
+		}
+
+		@Override
+		public final ST sourceType() {
+			return srcT;
+		}
+
+		@Override
+		public final TT targetType() {
+			return tgtT;
+		}
+
+		@Override
+		public final RET raw() {
+			return raw;
+		}
+
+		@Override
+		public final UniprotGraph<I, RV, RVT, RE, RET> graph() {
+			return UniprotGraph.this;
+		}
+	}
 
 
 }
--- src/main/java/com/bio4j/model/uniprot/nodes/Article.java
+++ src/main/java/com/bio4j/model/uniprot/nodes/Article.java
@@ -2,65 +2,59 @@ package com.bio4j.model.uniprot.nodes;
 
 import com.bio4j.model.uniprot.UniprotGraph;
 import com.bio4j.model.uniprot.relationships.ArticlePubmed;
+import com.bio4j.model.uniprot.relationships.ReferenceArticle;
 import com.ohnosequences.typedGraphs.Node;
 import com.ohnosequences.typedGraphs.Property;
+import com.ohnosequences.typedGraphs.UntypedGraph;
 
 import java.util.Date;
 
 /**
  * Created by ppareja on 7/23/2014.
  */
-public interface Article <
-		N extends Article<N, NT>,
-		NT extends UniprotGraph.ArticleType<N, NT>
-		>
-		extends Node<N, NT> {
+public final class Article <I extends UntypedGraph<RV, RVT, RE, RET>, RV, RVT, RE, RET>
+		extends UniprotGraph.UniprotVertex<
+		Article<I, RV, RVT, RE, RET>,
+		UniprotGraph<I, RV, RVT, RE, RET>.ArticleType,
+		I, RV, RVT, RE, RET
+		>  {
 
-	public String title();
-	public String date();
-	public String doiId();
+	public Article(RV vertex, UniprotGraph<I, RV, RVT, RE, RET>.ArticleType type) {
+		super(vertex, type);
+	}
+
+	@Override
+	public Article<I, RV, RVT, RE, RET> self() {
+		return this;
+	}
 
 	// properties
-
-		public static interface title<
-			N extends Article<N, NT>,
-			NT extends UniprotGraph.ArticleType<N, NT>,
-			P extends title<N, NT, P>
-			>
-			extends Property<N, NT, P, String> {
-		@Override
-		public default String name() {
-			return "title";
-		}
-		@Override
-		public default Class<String> valueClass() {
-			return String.class;
-		}
+	public String title() {
+		return get(type().title);
 	}
 
-	public static interface doiId<
-			N extends Article<N, NT>,
-			NT extends UniprotGraph.ArticleType<N, NT>,
-			P extends doiId<N, NT, P>
-			>
-			extends Property<N, NT, P, String> {
-		@Override
-		public default String name() {
-			return "doiId";
-		}
-		@Override
-		public default Class<String> valueClass() {
-			return String.class;
-		}
+	public String doId() {
+		return get(type().doId);
 	}
-
 
 	//////////////////////////////////////////////////////////////////////////////////////////////
 
 	// relationships
 
 	// articlePubmed
-	// ingoing
-	public <T extends ArticlePubmed> T articlePubmed_out();
-	public <T extends Pubmed> T articlePubmed_outNode();
+	// outgoing
+	public ArticlePubmed<I, RV, RVT, RE, RET>  articlePubmed_out(){
+		outOne(graph().ArticlePubmed());
+	}
+	public Pubmed<I, RV, RVT, RE, RET> articlePubmed_outNode(){
+		outOneV(graph().ArticlePubmed());
+	}
+	// referenceArticle
+	// outgoing
+	public ReferenceArticle<I, RV, RVT, RE, RET> referenceArticle_in(){
+		outOne(graph().ReferenceArticle());
+	}
+	public Reference<I, RV, RVT, RE, RET> referenceArticle_inNode(){
+		outOneV(graph().ReferenceArticle());
+	}
 }
