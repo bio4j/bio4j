cadb5243bdce2ffc4cf4e42523ea52500046b078
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-03-14 11:04:36 +0100

    clean import isoforms uniprot

--- src/main/java/com/bio4j/model/uniprot/programs/ImportIsoformSequences.java
+++ src/main/java/com/bio4j/model/uniprot/programs/ImportIsoformSequences.java
@@ -11,140 +11,136 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-
-/**
-* Imports the sequences of isoforms into Bio4j
-*
-* @author <a href="mailto:ppareja@era7.com">Pablo Pareja Tobes</a>
-*/
 public abstract class ImportIsoformSequences<I extends UntypedGraph<RV,RVT,RE,RET>,RV,RVT,RE,RET> {
 
   private static final Logger logger = Logger.getLogger("ImportIsoformSequences");
   private static FileHandler fh;
 
-  protected abstract UniProtGraph<I,RV,RVT,RE,RET> config(String dbFolder, String propertiesFile);
+  protected abstract UniProtGraph<I,RV,RVT,RE,RET> config(File dbFolder);
 
-  public void importIsoformSequences(String[] args) {
+  public void importIsoformSequences(File inFile, File dbFolder) {
 
-    if (args.length != 3) {
-      System.out.println("This program expects two parameters: \n"
-      + "1. Fasta file including all isoforms \n"
-      + "2. Bio4j DB folder \n"
-      + "3. DB Properties file (.properties)");
-    } else {
+    final long initTime = System.nanoTime();
 
-      long initTime = System.nanoTime();
+    int isoformCounter = 0;
 
-      File inFile = new File(args[0]);
-      String dbFolder = args[1];
-      String propertiesFile = args[2];
+    UniProtGraph<I,RV,RVT,RE,RET> uniProtGraph = config(dbFolder);
 
-      String isoformIdSt = null;
+    String isoformIdSt = null;
+    BufferedWriter statsBuff = null;
 
-      BufferedWriter statsBuff = null;
+    try {
 
-      UniProtGraph<I,RV,RVT,RE,RET> uniProtGraph = config(dbFolder, propertiesFile);
+      // This block configures the logger with handler and formatter
+      fh = new FileHandler("ImporIsoformSequences.log", true);
+      SimpleFormatter formatter = new SimpleFormatter();
+      fh.setFormatter(formatter);
+      logger.addHandler(fh);
+      logger.setLevel(Level.ALL);
 
-      int isoformCounter = 0;
+      statsBuff = new BufferedWriter(new FileWriter(new File("ImportIsoformSequencesStats.txt")));
+
+      BufferedReader reader = new BufferedReader(new FileReader(inFile));
+      String line;
+      StringBuilder seqStBuilder = new StringBuilder();
+
+      logger.log(Level.INFO, "updating isoforms data");
+
+      // this parses a fasta file I guess
+      while((line = reader.readLine()) != null) {
+
+        // got the header
+        if(line.trim().startsWith(">")) {
+
+          String[] columns = line.split("\\|");
+
+          isoformIdSt = columns[1];
+
+          String isoformNameSt = columns[2].split("OS=")[0].trim();
+
+          //sequence read line
+          line = reader.readLine();
+
+          // accumulate the sequence lines
+          // TODO I *think* that this will skip every other isoform
+          while (!line.trim().startsWith(">")) {
+
+            seqStBuilder.append(line);
+            line = reader.readLine();
+
+            if (line == null) {
+              break;
+            }
+          }
+
+          final String sequence = seqStBuilder.toString();
+          seqStBuilder.delete(0, seqStBuilder.length());
+
+          // TODO clean this a bit
+          uniProtGraph.isoformIdIndex()
+            .getVertex(isoformIdSt)
+            .ifPresent(
+              isoform -> {
+
+                isoform.set(uniProtGraph.Isoform().sequence, sequence);
+                isoform.set(uniProtGraph.Isoform().name, isoformNameSt);
+
+                // isoformCounter++;
+              }
+            );
+        }
+
+        if(line == null) {
+
+          break;
+        }
+      }
+
+      reader.close();
+
+      logger.log(Level.INFO, "Done! :)");
+
+    }
+    catch(Exception e) {
+
+      logger.log(Level.INFO, ("Exception retrieving isoform " + isoformIdSt));
+      logger.log(Level.SEVERE, e.getMessage());
+      StackTraceElement[] trace = e.getStackTrace();
+      for (StackTraceElement stackTraceElement: trace) {
+
+        logger.log(Level.SEVERE, stackTraceElement.toString());
+      }
+    }
+    finally {
+
+      uniProtGraph.raw().commit();
+
+      //closing logger file handler
+      fh.close();
+      logger.log(Level.INFO, "Closing up graph....");
+      // shutdown, makes sure all changes are written to disk
+      uniProtGraph.raw().shutdown();
 
       try {
 
-        // This block configures the logger with handler and formatter
-        fh = new FileHandler("ImporIsoformSequences.log", true);
-        SimpleFormatter formatter = new SimpleFormatter();
-        fh.setFormatter(formatter);
-        logger.addHandler(fh);
-        logger.setLevel(Level.ALL);
+        //-----------------writing stats file---------------------
+        long elapsedTime    = System.nanoTime() - initTime;
+        long elapsedSeconds = Math.round((elapsedTime / 1000000000.0));
+        long hours          = elapsedSeconds / 3600;
+        long minutes        = (elapsedSeconds % 3600) / 60;
+        long seconds        = (elapsedSeconds % 3600) % 60;
 
-        //---creating writer for stats file-----
-        statsBuff = new BufferedWriter(new FileWriter(new File("ImportIsoformSequencesStats.txt")));
+        statsBuff.write("Statistics for program ImportIsoformSequences:\nInput file: " + inFile.getName()
+        + "\nThere were " + isoformCounter + " isoforms updated.\n"
+        + "The elapsed time was: " + hours + "h " + minutes + "m " + seconds + "s\n");
 
-        BufferedReader reader = new BufferedReader(new FileReader(inFile));
-        String line;
-        StringBuilder seqStBuilder = new StringBuilder();
+        //---closing stats writer---
+        statsBuff.close();
 
-        logger.log(Level.INFO, "updating isoforms data....");
+      }
+      catch(Exception e) {
 
-        while ((line = reader.readLine()) != null) {
-          if (line.trim().startsWith(">")) {
-
-            String[] columns = line.split("\\|");
-            isoformIdSt = columns[1];
-            String isoformNameSt = columns[2].split("OS=")[0].trim();
-
-            //sequence read line
-            line = reader.readLine();
-
-            while (!line.trim().startsWith(">")) {
-              seqStBuilder.append(line);
-              line = reader.readLine();
-              if (line == null) {
-                break;
-              }
-            }
-
-            String sequence = seqStBuilder.toString();
-            seqStBuilder.delete(0, seqStBuilder.length());
-
-            Optional<Isoform<I,RV,RVT,RE,RET>> isoformOptional = uniProtGraph.isoformIdIndex().getVertex(isoformIdSt);
-
-            if(isoformOptional.isPresent()){
-              Isoform<I,RV,RVT,RE,RET> isoform = isoformOptional.get();
-              isoform.set(uniProtGraph.Isoform().sequence, sequence);
-              isoform.set(uniProtGraph.Isoform().name, isoformNameSt);
-              System.out.println("Setting name for: " + isoform.id());
-              isoformCounter++;
-            }
-
-          }
-
-          if (line == null) {
-            break;
-          }
-
-        }
-
-        reader.close();
-
-        logger.log(Level.INFO, "Done! :)");
-
-      } catch (Exception e) {
-        logger.log(Level.INFO, ("Exception retrieving isoform " + isoformIdSt));
-        logger.log(Level.SEVERE, e.getMessage());
-        StackTraceElement[] trace = e.getStackTrace();
-        for (StackTraceElement stackTraceElement : trace) {
-          logger.log(Level.SEVERE, stackTraceElement.toString());
-        }
-      } finally {
-
-        uniProtGraph.raw().commit();
-
-        //closing logger file handler
-        fh.close();
-        logger.log(Level.INFO, "Closing up graph....");
-        // shutdown, makes sure all changes are written to disk
-        uniProtGraph.raw().shutdown();
-
-        try {
-
-          //-----------------writing stats file---------------------
-          long elapsedTime = System.nanoTime() - initTime;
-          long elapsedSeconds = Math.round((elapsedTime / 1000000000.0));
-          long hours = elapsedSeconds / 3600;
-          long minutes = (elapsedSeconds % 3600) / 60;
-          long seconds = (elapsedSeconds % 3600) % 60;
-
-          statsBuff.write("Statistics for program ImportIsoformSequences:\nInput file: " + inFile.getName()
-          + "\nThere were " + isoformCounter + " isoforms updated.\n"
-          + "The elapsed time was: " + hours + "h " + minutes + "m " + seconds + "s\n");
-
-          //---closing stats writer---
-          statsBuff.close();
-
-        }catch(Exception e){
-          e.printStackTrace();
-        }
-
+        e.printStackTrace();
       }
     }
   }
