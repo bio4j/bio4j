ca073aeb1bea1106f9da0428179ef755ee2615f6
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-03-15 12:38:25 +0100

    clean protein interaction edges

--- src/main/java/com/bio4j/model/uniprot/edges/ProteinIsoformInteraction.java
+++ src/main/java/com/bio4j/model/uniprot/edges/ProteinIsoformInteraction.java
@@ -5,34 +5,19 @@ import com.bio4j.model.uniprot.vertices.Isoform;
 import com.bio4j.model.uniprot.vertices.Protein;
 import com.bio4j.angulillos.UntypedGraph;
 
-/**
-* Created by ppareja on 9/23/2014.
-*/
 public class ProteinIsoformInteraction<I extends UntypedGraph<RV, RVT, RE, RET>, RV, RVT, RE, RET>
-extends
-UniProtGraph.UniProtEdge<
-Protein<I, RV, RVT, RE, RET>, UniProtGraph<I, RV, RVT, RE, RET>.ProteinType,
-ProteinIsoformInteraction<I, RV, RVT, RE, RET>, UniProtGraph<I, RV, RVT, RE, RET>.ProteinIsoformInteractionType,
-Isoform<I, RV, RVT, RE, RET>, UniProtGraph<I, RV, RVT, RE, RET>.IsoformType,
-I, RV, RVT, RE, RET
-> {
+  extends
+    UniProtGraph.UniProtEdge<
+      Protein<I, RV, RVT, RE, RET>, UniProtGraph<I, RV, RVT, RE, RET>.ProteinType,
+      ProteinIsoformInteraction<I, RV, RVT, RE, RET>, UniProtGraph<I, RV, RVT, RE, RET>.ProteinIsoformInteractionType,
+      Isoform<I, RV, RVT, RE, RET>, UniProtGraph<I, RV, RVT, RE, RET>.IsoformType,
+      I, RV, RVT, RE, RET
+    >
+{
 
   public ProteinIsoformInteraction(RE edge, UniProtGraph<I, RV, RVT, RE, RET>.ProteinIsoformInteractionType type) {
-
     super(edge, type);
   }
-
-  // properties
-  // public String experiments() {
-  //   return get(type().experiments);
-  // }
-  // public String organismsDiffer() {  return get(type().organismsDiffer);  }
-  // public String intActId1() {
-  //   return get(type().intActId1);
-  // }
-  // public String intActId2() {
-  //   return get(type().intActId2);
-  // }
 
   @Override
   public ProteinIsoformInteraction<I, RV, RVT, RE, RET> self() {
