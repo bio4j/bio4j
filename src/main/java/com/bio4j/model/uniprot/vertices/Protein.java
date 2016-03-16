440dcb8863b298db142286936c4168a0f71d5530
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-03-16 11:41:33 +0100

    clean imports in Protein vertex

--- src/main/java/com/bio4j/model/uniprot/vertices/Protein.java
+++ src/main/java/com/bio4j/model/uniprot/vertices/Protein.java
@@ -1,22 +1,7 @@
 package com.bio4j.model.uniprot.vertices;
 
-import com.bio4j.model.enzymedb.vertices.Enzyme;
-import com.bio4j.model.go.vertices.GoTerm;
-import com.bio4j.model.ncbiTaxonomy.vertices.NCBITaxon;
 import com.bio4j.model.uniprot.UniProtGraph;
-import com.bio4j.model.uniprot.edges.*;
-import com.bio4j.model.uniprot_enzymedb.edges.EnzymaticActivity;
-import com.bio4j.model.uniprot_go.edges.GoAnnotation;
-import com.bio4j.model.uniprot_ncbiTaxonomy.edges.ProteinNCBITaxon;
-import com.bio4j.model.uniprot_uniref.edges.*;
-import com.bio4j.model.uniref.vertices.UniRef100Cluster;
-import com.bio4j.model.uniref.vertices.UniRef50Cluster;
-import com.bio4j.model.uniref.vertices.UniRef90Cluster;
 import com.bio4j.angulillos.UntypedGraph;
-
-import java.util.Date;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 public final class Protein <I extends UntypedGraph<RV, RVT, RE, RET>, RV, RVT, RE, RET>
   extends
@@ -27,12 +12,8 @@ public final class Protein <I extends UntypedGraph<RV, RVT, RE, RET>, RV, RVT, R
     >
 {
 
-  public Protein(RV vertex, UniProtGraph<I, RV, RVT, RE, RET>.ProteinType type) {
-    super(vertex, type);
-  }
+  public Protein(RV vertex, UniProtGraph<I, RV, RVT, RE, RET>.ProteinType type) { super(vertex, type); }
 
   @Override
-  public Protein<I, RV, RVT, RE, RET> self() {
-    return this;
-  }
+  public Protein<I, RV, RVT, RE, RET> self() { return this; }
 }
