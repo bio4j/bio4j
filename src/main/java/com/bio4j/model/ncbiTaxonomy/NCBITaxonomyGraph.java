b5e7b0c81a4f96b57e3b5fa7004e1da537fb21fa
Alexey Alekhin <laughedelic@gmail.com>
2016-08-15 21:16:24 +0200

    Reviewed NCBI Taxonomy graph; Ordered schema declarations: - The value-type goes first because it has a fixed structure: its constructor and the self-reference, while the type-type can have multiple properties inside and its declaration may span across many lines so that when you view it or write, the value-type thing gets out of the visible scope - Same reason for putting `FooType foo` value declaration right in front of the `FooType` - Same reason for putting `Foo fromRaw` before properties - `@Override` on the same line as the method, because these lines are pure boilerplate that is not useful when you are reading the code. So this way they occupy less space and don't distract you attention too much

--- src/main/java/com/bio4j/model/ncbiTaxonomy/NCBITaxonomyGraph.java
+++ src/main/java/com/bio4j/model/ncbiTaxonomy/NCBITaxonomyGraph.java
@@ -19,68 +19,66 @@ import com.bio4j.angulillos.Arity.*;
 
 public final class NCBITaxonomyGraph<V,E> extends TypedGraph<NCBITaxonomyGraph<V,E>,V,E> {
 
+  public NCBITaxonomyGraph(UntypedGraph<V,E> graph) { super(graph); }
+  @Override public final NCBITaxonomyGraph<V,E> self() { return this; }
+
   /*
     ## Vertices
 
     There is a single type of vertex, a taxon.
   */
-  /*
-    ### Taxon
-  */
+
+  /* ### Taxon */
+  public final class Taxon extends Vertex<Taxon> {
+
+    private Taxon(V vertex) { super(vertex, taxon); }
+    @Override public final Taxon self() { return this; }
+  }
+
+  public final TaxonType taxon = new TaxonType();
   public final class TaxonType extends VertexType<Taxon> {
 
-    /*
-      #### id
-    */
+    @Override public final Taxon fromRaw(V vertex) { return new Taxon(vertex); }
+
+    /* #### Id */
+    public final Id id = new Id();
     public final class Id extends Property<String> implements FromAtMostOne, ToOne {
 
       private Id() { super(String.class); }
     }
-    public final Id id = new Id();
 
     /*
       #### ById
 
-      An unique index for taxa by id.
+      A unique index for taxa by id.
     */
-    public final class ById extends UniqueIndex<Id,String> {
+    public final ById byId = new ById();
+    public final class ById extends UniqueIndex<Id, String> {
 
       private ById() { super(id); }
     }
-    public final ById byId = new ById();
 
     /*
-      #### name
+      #### Name
 
       This is the standard name of the taxon, like *Escherichia coli* or *Bacteria*.
     */
+    public final Name name = new Name();
     public final class Name extends Property<String> implements FromAny, ToOne {
 
       private Name() { super(String.class); }
     }
-    public final Name name = new Name();
 
     /*
-      #### taxonomic rank
+      #### Taxonomic rank
 
       Values are, among others, *species*, *genus*, or *no rank*.
     */
+    public final TaxonomicRank taxonomicRank = new TaxonomicRank();
     public final class TaxonomicRank extends Property<TaxonomicRanks> implements FromAny, ToOne {
 
       private TaxonomicRank() { super(TaxonomicRanks.class); }
     }
-    public final TaxonomicRank taxonomicRank = new TaxonomicRank();
-
-    @Override
-    public final Taxon fromRaw(V vertex) { return new Taxon(vertex); }
-  }
-  public final TaxonType taxon = new TaxonType();
-
-  public final class Taxon extends Vertex<Taxon> {
-
-    private Taxon(V vertex) { super(vertex, taxon); }
-    @Override
-    public final Taxon self() { return this; }
   }
 
   public static enum TaxonomicRanks {
@@ -96,29 +94,22 @@ public final class NCBITaxonomyGraph<V,E> extends TypedGraph<NCBITaxonomyGraph<V
 
     This graph has only one edge type, `parent`.
   */
+
   /*
     ### Parent
 
     Every taxon *but* the root has *exactly one* parent.
   */
-  public final class ParentType extends EdgeType<Taxon, Parent, Taxon> implements FromAny, ToAtMostOne {
-
-    private ParentType() { super(taxon, taxon); }
-
-    @Override
-    public final Parent fromRaw(E edge) { return new Parent(edge); }
-  }
-  public final ParentType parent = new ParentType();
-
   public final class Parent extends Edge<Taxon, Parent, Taxon> {
 
     private Parent(E edge) { super(edge, parent); }
-    @Override
-    public final Parent self() { return this; }
+    @Override public final Parent self() { return this; }
   }
 
-  public NCBITaxonomyGraph(UntypedGraph<V,E> graph) { super(graph); }
+  public final ParentType parent = new ParentType();
+  public final class ParentType extends EdgeType<Taxon, Parent, Taxon> implements FromAny, ToAtMostOne {
 
-  @Override
-  public final NCBITaxonomyGraph<V,E> self() { return this; }
+    private ParentType() { super(taxon, taxon); }
+    @Override public final Parent fromRaw(E edge) { return new Parent(edge); }
+  }
 }
