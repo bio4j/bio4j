02ea0cd25a840309f64e45d5ee728fb8c09f0f5e
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-09-27 11:20:52 +0200

    write general UniRef docs

--- src/main/java/com/bio4j/model/UniRefGraph.java
+++ src/main/java/com/bio4j/model/UniRefGraph.java
@@ -1,8 +1,34 @@
+/*
+  # UniRef
+
+  [UniRef](http://www.uniprot.org/help/uniref) clusters protein sequences from UniProt and UniParc based (among other criteria) on sequence similarity. There are three different partitions: UniRef100, UniRef90 and UniRef50, where the numbered suffix is the "identity" threshold used for the clustering procedure.
+
+  1. **UniRef100** clusters contain identical sequences *and* substrings from one of the organisms of the cluster, depending on the order in which the sequences are processed.
+  2. UniRef90 clusters are *derived* from UniRef100 representatives, but their corresponding representatives are not; UniRef90 representatives are chosen between each cluster member through different criteria.
+  3. UniRef50 clusters are **derived* from UniRef90 representatives, but their corresponding representatives are not; UniRef50 representatives are chosen between each cluster member through different criteria.
+
+  The average and maximum cluster size for UniRef50 (the one with the biggest clusters) are approximately `10` and `10^6`.
+
+  If a cluster (of any paritition) contains a UniProt protein the its representative will be a UniProt protein. Note that the selection of representatives and the update procedures are somewhat arbitrary; quoting from the [UniRef clusters: a comprehensive and scalable alternative for improving sequence similarity searches](https://bioinformatics.oxfordjournals.org/content/31/6/926.full):
+
+  > Using a full update procedure, the clusters are computed ab initio at the end of year and are updated for the remaining year using an incremental procedure that favors clustering of new sequences under existing clusters. The representatives of clusters are selected based on the level of curation (reviewed versus unreviewed), protein name (e.g. names do not contain hypothetical or putative preferred), source organism (e.g. proteins from model organisms preferred) and length of protein. The UniRef identifiers are derived from the cluster ‘representatives’ identifiers and are preserved for approximately 98% of the clusters between releases.
+
+  ## UniRef and UniProt
+
+  In Bio4j we only include clusters for which the representative is a UniProt protein.
+
+  ## References
+
+  - [UniRef100 FTP Readme](ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref100/README)
+  - [UniRef90 FTP Readme](ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref90/README)
+  - [UniRef50 FTP Readme](ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref50/README)
+  - [UniRef clusters: a comprehensive and scalable alternative for improving sequence similarity searches](https://bioinformatics.oxfordjournals.org/content/31/6/926.full)
+  - [UniRef help](http://www.uniprot.org/help/uniref)
+*/
 package com.bio4j.model;
 
 import com.bio4j.angulillos.*;
 import com.bio4j.angulillos.Arity.*;
-import java.util.Set;
 
 public final class UniRefGraph<V,E> extends LinkGraph<UniRefGraph<V,E>,V,E> {
 
@@ -58,4 +84,6 @@ public final class UniRefGraph<V,E> extends LinkGraph<UniRefGraph<V,E>,V,E> {
     private UniRef100RepresentativeType() { super(uniRef100Cluster, uniProtGraph.protein); }
     @Override public final UniRef100Representative fromRaw(E edge) { return new UniRef100Representative(edge); }
   }
+
+
 }
