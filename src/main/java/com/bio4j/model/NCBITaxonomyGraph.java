93cba13fc2fe1c432d0a07cb50fedcf4bfe9bc62
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-10-03 17:12:29 +0200

    update NCBI taxonomy, docs

--- src/main/java/com/bio4j/model/NCBITaxonomyGraph.java
+++ src/main/java/com/bio4j/model/NCBITaxonomyGraph.java
@@ -1,16 +1,7 @@
 /*
-  # NCBI Taxonomy graph
+  # NCBI Taxonomy
 
-  This graph models the NCBI taxonomy tree.
-
-  > This documentation will be part of the import code, which is going to be in a separate repository
-
-  Files used in the importing process can be found [here](ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz)
-
-  Once that information is extracted we are building the tree from the information included in the following files:
-
-  * **nodes.dmp**
-  * **names.dmp**
+  Taxon vertices, with a *parent* edge corresponding to the tree structure.
  */
 package com.bio4j.model;
 
@@ -23,15 +14,13 @@ public final class NCBITaxonomyGraph<V,E> extends TypedGraph<NCBITaxonomyGraph<V
   @Override public final NCBITaxonomyGraph<V,E> self() { return this; }
 
   /*
-    ## Vertices
+    ## Taxon
 
-    There is a single type of vertex, a taxon.
   */
-
-  /* ### Taxon */
   public final class Taxon extends Vertex<Taxon> {
 
     private Taxon(V vertex) { super(vertex, taxon); }
+
     @Override public final Taxon self() { return this; }
   }
 
@@ -40,28 +29,27 @@ public final class NCBITaxonomyGraph<V,E> extends TypedGraph<NCBITaxonomyGraph<V
 
     @Override public final Taxon fromRaw(V vertex) { return new Taxon(vertex); }
 
-    /* #### Id */
-    public final Id id = new Id();
-    public final class Id extends Property<String> implements FromAtMostOne, ToOne {
-
-      private Id() { super(String.class); }
-    }
-
     /*
-      #### ById
+      ### ID
 
-      A unique index for taxa by id.
+      Indexed for unique matches.
     */
-    public final ById byId = new ById();
-    public final class ById extends UniqueIndex<Id, String> {
+    public final ID id = new ID();
+    public final class ID extends Property<String> implements FromAtMostOne, ToOne {
 
-      private ById() { super(id); }
+      private ID() { super(String.class); }
+
+      public final Index index = new Index();
+      public final class Index extends UniqueIndex<ID, String> {
+
+        private Index() { super(id); }
+      }
     }
 
     /*
-      #### Name
+      ### Name
 
-      This is the standard name of the taxon, like *Escherichia coli* or *Bacteria*.
+      This is the standard name of the taxon, like *Escherichia coli* or *Bacteria*. Corresponds the the (non-unique) *scientific name* in NCBI taxonomy data.
     */
     public final Name name = new Name();
     public final class Name extends Property<String> implements FromAny, ToOne {
@@ -70,9 +58,9 @@ public final class NCBITaxonomyGraph<V,E> extends TypedGraph<NCBITaxonomyGraph<V
     }
 
     /*
-      #### Taxonomic rank
+      ### Taxonomic rank
 
-      Values are, among others, *species*, *genus*, or *no rank*.
+      Values are, among others, *species*, *genus*, or *no rank*; see the enum below.
     */
     public final TaxonomicRank taxonomicRank = new TaxonomicRank();
     public final class TaxonomicRank extends Property<TaxonomicRanks> implements FromAny, ToOne {
@@ -83,26 +71,37 @@ public final class NCBITaxonomyGraph<V,E> extends TypedGraph<NCBITaxonomyGraph<V
 
   public static enum TaxonomicRanks {
 
-    // TODO add everything here
-    species,
+    noRank,
+    superkingdom,
+    kingdom,
+    superphylum,
+    phylum,
+    subphylum,
+    clazz, // reserved word
+    subclass,
+    order,
+    suborder,
+    family,
+    subfamily,
+    tribe,
+    subtribe,
     genus,
-    noRank;
+    subgenus,
+    speciesGroup,
+    speciesSubgroup,
+    species,
+    subspecies;
   }
 
   /*
-    ## Edges
-
-    This graph has only one edge type, `parent`.
-  */
-
-  /*
-    ### Parent
+    ### Parent taxon
 
     Every taxon *but* the root has *exactly one* parent.
   */
   public final class Parent extends Edge<Taxon, Parent, Taxon> {
 
     private Parent(E edge) { super(edge, parent); }
+    
     @Override public final Parent self() { return this; }
   }
 
